885b85b0a954584943783795d97b8bd402152395
Andy Wang <zxcvbn884488@gmail.com>
2021-12-20 21:08:37 +0800

    use class

--- example.py
+++ example.py
@@ -1,16 +1,16 @@
 # to be sorted
 stringsList = [
     'apply',
-    'balance',
-    'count',
     'drop',
+    'count',
+    'balance',
 ]
 # sort all strings
 """ stringsList = [
     'alppy',
-    'aabceln',
-    'cnotu',
     'dopr',
+    'cnotu',
+    'aabceln',
 ] """
 # sort array by strings
 """ stringsList = [
@@ -19,65 +19,66 @@ stringsList = [
     'cnotu',
     'dopr',
 ] """
+# sorting strings in list
+class sorting:
+    def mergeSort(list):
+        # 分割：到每列表只剩 1 元素為止
+        if len(list) > 1:
+            mid = len(list) // 2
+            left_list = list[:mid] # 0 ~ mid - 1 => Left
+            right_list = list[mid:] # mid ~ end => Right
 
-def mergeSort(list):
-    # 分割：到每列表只剩 1 元素為止
-    if len(list) > 1:
-        mid = len(list) // 2
-        left_list = list[:mid] # 0 ~ mid - 1 => Left
-        right_list = list[mid:] # mid ~ end => Right
+            sorting.mergeSort(left_list)
+            sorting.mergeSort(right_list)
 
-        mergeSort(left_list)
-        mergeSort(right_list)
+            # 整合
+            right_index = 0
+            left_index = 0
+            merged_index = 0
+            # 從兩列表的第一項開始比較大小，依序往後比較
+            while right_index < len(right_list) and left_index < len(left_list):
+                # 較小的數值先放入最終的合併列表中
+                if right_list[right_index] < left_list[left_index]:
+                    list[merged_index] = right_list[right_index]
+                    # 被放入的數值的列表的 index 往後一位
+                    right_index = right_index + 1
+                else:
+                    list[merged_index] = left_list[left_index]
+                    left_index = left_index + 1
+                # 合併列表的 index 往後一位
+                merged_index = merged_index + 1
 
-        # 整合
-        right_index = 0
-        left_index = 0
-        merged_index = 0
-        # 從兩列表的第一項開始比較大小，依序往後比較
-        while right_index < len(right_list) and left_index < len(left_list):
-            # 較小的數值先放入最終的合併列表中
-            if right_list[right_index] < left_list[left_index]:
+            # 放入右列表剩餘項
+            while right_index < len(right_list):
                 list[merged_index] = right_list[right_index]
-                # 被放入的數值的列表的 index 往後一位
                 right_index = right_index + 1
-            else:
+                merged_index = merged_index + 1
+
+            # 放入左列表剩餘項
+            while left_index < len(left_list):
                 list[merged_index] = left_list[left_index]
                 left_index = left_index + 1
-            # 合併列表的 index 往後一位
-            merged_index = merged_index + 1
+                merged_index = merged_index + 1
 
-        # 放入右列表剩餘項
-        while right_index < len(right_list):
-            list[merged_index] = right_list[right_index]
-            right_index = right_index + 1
-            merged_index = merged_index + 1
-
-        # 放入左列表剩餘項
-        while left_index < len(left_list):
-            list[merged_index] = left_list[left_index]
-            left_index = left_index + 1
-            merged_index = merged_index + 1
-
-def mergeSortWithDifferentType(toBeSorted):
-    varType = type(toBeSorted)
-    # 排序字串
-    if varType == str:
-        toBeSorted = list(toBeSorted)
-        mergeSort(toBeSorted)
-        toBeSorted = "".join(toBeSorted)
-    # 排序列表，先兩兩比較 string 後再排序
-    elif varType == list:
-        mergeSort(toBeSorted)
-    return toBeSorted
+    def mergeSortWithDifferentType(toBeSorted):
+        varType = type(toBeSorted)
+        # 排序字串
+        if varType == str:
+            toBeSorted = list(toBeSorted)
+            sorting.mergeSort(toBeSorted)
+            toBeSorted = "".join(toBeSorted)
+        # 排序列表，先兩兩比較 string 後再排序
+        elif varType == list:
+            sorting.mergeSort(toBeSorted)
+        return toBeSorted
 
 
 def sortStringsInList(stringsList):
     for string in stringsList:
         stringIndex = stringsList.index(string)
-        stringsList[stringIndex] = mergeSortWithDifferentType(string)
+        stringsList[stringIndex] = sorting.mergeSortWithDifferentType(string)
 
-    mergeSort(stringsList)
+    sorting.mergeSort(stringsList)
 
     return stringsList
     
