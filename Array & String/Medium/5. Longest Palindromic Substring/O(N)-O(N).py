6d63916e300c434f48b340d5b26448ac00039706
Andy Wang <zxcvbn884488@gmail.com>
2022-02-20 21:44:31 +0800

    fix: modify comment

--- Array & String/Medium/5. Longest Palindromic Substring/O(N)-O(N).py
+++ Array & String/Medium/5. Longest Palindromic Substring/O(N)-O(N).py
@@ -7,33 +7,33 @@ class Solution:
       # abba -> ^a#b#b#a$
       S = "#".join("^" + s + "$")
       # 初始化回文的長度
-      P = [0] * len(S) # 以 S 每個位置為中心的最長半徑的回文
+      P = [0] * len(S) # 以 S 每個位置為中心點，取得該位置的最長回文半徑
       # 備註：len(S) = len(P) = 2 * len(s) + 1
 
-      # center 回文的中心點，radius 回文的半徑
-      center, radius = 0, 0
+      # center 回文的中心點，right 回文的右半邊界
+      center, right = 0, 0
 
-      # 檢查每個位置的回文半徑
+      # 檢查每個位置的回文右半邊界
       for i in range(1, len(S) - 1): # range(1, len(S) - 1)：因為 S 的第一個字元是 ^，最後一個字元是 $
-         # 如果 i < right，表示 i 已經被檢查過了，不需要再檢查
-         if i < radius:
-            P[i] = min(radius - i, P[2 * center - i])
+         # 如果 i < right，P[i] 為 P[center] 的子回文
+         if i < right:
+            P[i] = min(right - i, P[2 * center - i])
 
-         # 檢查以 i 為中心點，在半徑 P[i] + 1 內是否為回文
+         # 檢查以 i 為中心點，在左右邊界 P[i] + 1 內是否為回文
          while S[i + P[i] + 1] == S[i - P[i] - 1]:
             # P[i]：該中心點 i 的最長回文半徑
             P[i] += 1
 
-         # 更新回文的中心點及半徑
-         if i + P[i] > radius:
-            center, radius = i, i + P[i]
+         # 更新回文的中心點及右半邊界
+         if i + P[i] > right:
+            center, right = i, i + P[i]
 
       # 最長回文的半徑與中心點索引
       p, idx = max((v, i) for i, v in enumerate(P))
 
       return s[ (idx - p) // 2 : (idx + p) // 2 ] # // 2 是為了將 # 排除在外
 
-string = "abccbaaaa"
+string = "abccbaaaa" # ^a#b#c#c#b#a#a#a#a$
 result = Solution().longestPalindrome(string)
 print(result)
 
